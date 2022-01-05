f3e38963485dec12e49cafc3c4bd7158e60af198
Samuel Lüthi <samuel.luethi@8460.ch>
2022-01-05 12:48:14 +0100

    Update WS2812b.h

--- easyWS2812_lib/WS2812b.h
+++ easyWS2812_lib/WS2812b.h
@@ -1,22 +1,33 @@
 /************************************************************************************
-*	Projekt:	LED_Controller_							 _.-^^---....,,--_			*
-*	Autor:		Samuel Lüthi						 _--*%ç&*ç&*%&*%&*&*bb--_		*
-*	Datum:		16.12.2021							<#ç``+@¬|( ´~¢|¬(%&¬|fegh>)		*
-*												    |tradgrtgrt67584322423h6jp|		*
-*													 \._ .;7gi&/&%hu89o99$8$_./		*
-*														```--. . , ; .--'''			*
-*															  | |   |				*
-*														   .-=||  | |=-.			*
-*														   `-=#$%&%$#=-'			*
-*															  | ;  :|				*
-*													 _____.,-#%&$@%#&#~,._____		*
+*	Projekt:	LED_Controller_			                        	 _.-^^---....,,--_	  		*
+*	Autor:		Samuel LÃ¼thi	          			              _--*%Ã§&*Ã§&*%&*%&*&*bb--_		*
+*	Datum:		16.12.2021					                  		<#Ã§``+@Â¬|( Â´~Â¢|Â¬(%&Â¬|fegh>)		*
+*												                              |tradgrtgrt67584322423h6jp|		*
+*													                             \._ .;7gi&/&%hu89o99$8$_./		*
+*														                               ```--. . , ; .--'''			*
+*															                                   | |   |	    			*
+*														                                  .-=||  | |=-.		    	*
+*														                                  `-=#$%&%$#=-'			    *
+*															                                   | ;  :|				    *
+*													                              _____.,-#%&$@%#&#~,._____		*
 ************************************************************************************/
+#ifdef SIXTEEN
 
+	#define NOPON asm("nop\n\tnop\n\t");
+	#define NOPOFF asm("nop\n\t");
+#endif
+
+#ifdef EIGHT
+
+#define NOPON asm("nop\n\t");
+#define NOPOFF 
+
+#endif
 
 /*** System Variablen ***/
-unsigned char i;							// Zählvariable für For-Schleifen
+unsigned char i;							// ZÃ¤hlvariable fÃ¼r For-Schleifen
 
 /***function prototypes***/
 void LED_WRITE( unsigned int, unsigned char, unsigned char, unsigned char );							//write color to stripe
-unsigned char HSV_to_RGB(unsigned int, unsigned char, unsigned char, unsigned char, unsigned int );				//convert HSV to RGB
+uint32_t HSV_to_RGB(unsigned int, unsigned char, unsigned char, unsigned char, unsigned int );				//convert HSV to RGB
 
